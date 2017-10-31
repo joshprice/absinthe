573962f6039fa49977fd05aa029b0d210dac74c8
Josh Price <josh@alembic.com.au>
2017-10-31 17:19:09 +1100

    Failing test for detecting output objects nested in input objects

--- test/lib/absinthe/schema/rule/input_output_types_correctly_placed_test.exs
+++ test/lib/absinthe/schema/rule/input_output_types_correctly_placed_test.exs
@@ -10,7 +10,9 @@ defmodule Absinthe.Schema.Rule.InputOuputTypesCorrectlyPlacedTest do
       assert_schema_error("invalid_input_types",
                           [
                             %{rule: Rule.InputOuputTypesCorrectlyPlaced, data: %{argument: :invalid_arg, struct: Absinthe.Type.Object, type: :user}},
+                            %{rule: Rule.InputOuputTypesCorrectlyPlaced, data: %{argument: :invalid_nested_arg, struct: Absinthe.Type.InputObject, type: :bad_input_object}},
                             %{rule: Rule.InputOuputTypesCorrectlyPlaced, data: %{field: :blah, parent: Absinthe.Type.Object, struct: Absinthe.Type.InputObject, type: :input}},
+                            %{rule: Rule.InputOuputTypesCorrectlyPlaced, data: %{field: :person, parent: Absinthe.Type.InputObject, struct: Absinthe.Type.Object, type: :user}},
                           ]
       )
     end
--- test/support/schemas/invalid_input_types.exs
+++ test/support/schemas/invalid_input_types.exs
@@ -5,16 +5,20 @@ defmodule Absinthe.Test.InvalidInputTypes do
   end
 
   input_object :input do
-
   end
 
   object :bad_object do
     field :blah, :input
   end
 
+  input_object :bad_input_object do
+    field :person, :user
+  end
+
   query do
     field :foo, :user do
       arg :invalid_arg, :user
+      arg :invalid_nested_arg, :bad_input_object
     end
   end
 
